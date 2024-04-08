94ede9a0daf99153cbdc5702a20266bfb416fa3e
Opilane TTHK <opilane@tthk.ee>
2024-04-08 10:40:39 +0300

    a
    a

---
+++ FilmsMAUI/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Maui;
+using FilmsMAUI.Services;
+using Microsoft.Extensions.Logging;
+
+namespace FilmsMAUI;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
+				fonts.AddFont("Poppins-Semibold.ttf", "PoppinsSemibold");
+			});
+
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddHttpClient(TmdbService.TmdbHttpClientName,
+            HttpClient => HttpClient.BaseAddress = new Uri("https://api.themoviedb.org"));
+
+        return builder.Build();
+    }
+}
---
+++ FilmsMAUI/Services/TmdbService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmsMAUI.Services
+{
+    public class TmdbService
+    {
+        private const string ApiKey = "";
+        public const string TmdbHttpClientName = "TmdbClient";
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TmdbService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private HttpClient HttpClient => _httpClientFactory.CreateClient(TmdbHttpClientName);
+
+
+    }
+    public static class TmdbUrls
+    {
+        public const string Trending = "3/trending/all/week?language=en-US";
+        public const string NetflixOriginals = "3/discover/tv?language=en-US&with_networks=213";
+        public const string TopRated = "3/movie/top_rated?language=en-US";
+        public const string Action = "3/discover/movie?language=en-US&swith_generes=28";
+
+    }
+}
